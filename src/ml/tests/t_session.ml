f7985af34f147109ff62d7b12f6a582f37d35a45
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-02-13 16:17:53 -0500

    format

--- src/ml/tests/t_session.ml
+++ src/ml/tests/t_session.ml
@@ -1,11 +1,8 @@
 module C = Imandrax_api_client
 
 let pf = Format.printf
-
 let client = C.connect_tcp_exn @@ C.addr_inet_local ()
-
 let session : C.API.session = C.Session.create client |> C.Fut.wait_block_exn
-
 let () = pf "got session@."
 
 (* let () = pf "session=%S@." (Bytes.unsafe_to_string session.id) *)
@@ -13,5 +10,4 @@ let () = pf "got session@."
 (* let () = Format.printf "GC info: %a\n%!" C.API.pp_gc_stats _gc_stats *)
 
 let () = C.disconnect client
-
 let () = pf "disconnected@."
