75fb39a776ece94210635dc7c52815ba2169c40b
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-02-16 11:39:22 -0500

    migrate to ppx_subliner exclusively

--- src/ml/cli/cli_opts_.ml
+++ src/ml/cli/cli_opts_.ml
@@ -12,7 +12,7 @@ type conn = {
   port: int option;
   debug: bool;  (** Enable debug *)
 }
-[@@deriving cmdliner, show { with_path = false }]
+[@@deriving subliner, show { with_path = false }]
 (** Show imports by calling `dot`. *)
 
 type conn_with_repeat = {
@@ -20,17 +20,18 @@ type conn_with_repeat = {
   debug: bool;  (** Enable debug *)
   repeat: float option;  (** Repeat every [repeat] seconds *)
 }
-[@@deriving cmdliner, show { with_path = false }]
+[@@deriving subliner, show { with_path = false }]
 (** Show imports by calling `dot`. *)
 
 type repl = {
   port: int option;
   debug: bool; [@default false] [@aka [ "d" ]]  (** Enable debug *)
-  log_level: Logs.level option; [@conv parse_log_level]  (** Log level *)
+  log_level: (Logs.level[@conv parse_log_level]) option; [@conv parse_log_level]
+      (** Log level *)
   log_file: string option;
   session: string option;  (** Session ID *)
   raw: bool;  (** Disable linenoise *)
   bt: bool;  (** backtraces *)
 }
-[@@deriving cmdliner, show { with_path = false }]
+[@@deriving subliner, show { with_path = false }]
 (** Run the ImandraX REPL. *)
