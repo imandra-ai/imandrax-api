1375091ee4d718dda78f4efcf477a66d4617ba31
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-07-30 10:28:53 -0400

    gitignore, progress in python API lib

--- tests/api/py/test1.py
+++ tests/api/py/test1.py
@@ -1,24 +1,33 @@
-from imandrax_api.bindings import utils_pb2, simple_api_twirp, simple_api_pb2
-from twirp.context import Context
+import imandrax_api
 
-c = simple_api_twirp.SimpleClient("http://localhost:8082")
-c.status(ctx=Context(), server_path_prefix="/api/v1", request=utils_pb2.Empty())
+c = imandrax_api.Client(url="http://localhost:8082")
+c.status()
 
-sesh = c.create_session(
-    ctx=Context(), server_path_prefix="/api/v1", request=utils_pb2.Empty()
-)
+x1 = c.eval_src(src="let f x=x+1 ;;")
+print("x1: ", x1)
 
-x1 = c.eval_src(
-    ctx=Context(),
-    server_path_prefix="/api/v1",
-    request=simple_api_pb2.EvalSrcReq(src="let f x=x+1 ;;", session=sesh),
-)
-print('x1: ', x1)
+x2 = c.eval_src(src="theorem yolo1 x = f x > x ;;")
 
-x2 = c.eval_src(
-    ctx=Context(),
-    server_path_prefix="/api/v1",
-    request=simple_api_pb2.EvalSrcReq(src="theorem yolo1 x = f x > x ;;", session=sesh),
-)
+print("x2: ", x2)
 
-print('x2: ', x2)
+print(x2.tasks)
+task2 = x2.tasks[0]
+print("task2: ", task2)
+
+arts = c.list_artifacts(task2)
+print("arts:", arts)
+
+art_show = c.get_artifact_zip(task2, kind="show")
+print(art_show)
+
+art_po_task = c.get_artifact_zip(task2, kind="po_task")
+print(art_po_task)
+
+with open('art.po_task.zip', 'wb') as f:
+    f.write(art_po_task.art_zip)
+
+art_po_res = c.get_artifact_zip(task2, kind="po_res")
+print(art_po_res)
+
+with open('art.po_res.zip', 'wb') as f:
+    f.write(art_po_res.art_zip)
