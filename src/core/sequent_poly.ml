ef29346f29b1e0801a1a6646b9677705feb97bc0
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-07-30 12:33:08 -0400

    more printers

--- src/core/sequent_poly.ml
+++ src/core/sequent_poly.ml
@@ -4,5 +4,17 @@ type 'term t = {
   hyps: 'term list;  (** Hypotheses *)
   concls: 'term list;  (** Conclusions *)
 }
-[@@deriving eq, ord, twine, typereg, map, iter, show { with_path = false }]
+[@@deriving eq, ord, twine, typereg, map, iter]
 (** A classical sequent *)
+
+let pp ppt out (self : _ t) : unit =
+  if self.hyps = [] then
+    Fmt.fprintf out "`@[|- %a@]`" (Util.pp_list ~sep:"," ppt) self.concls
+  else if self.hyps = [] then
+    Fmt.fprintf out "`@[%a |- %a@]`"
+      (Util.pp_list ~sep:"," ppt)
+      self.hyps
+      (Util.pp_list ~sep:"," ppt)
+      self.concls
+
+let show ppt = Fmt.to_string @@ pp ppt
