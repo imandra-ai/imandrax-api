76a6e08542ca421169b892022dc587fdf039e5d4
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-09-09 10:04:32 -0400

    codegen: trim prelude

--- src/proof-system/codegen/ocaml_prelude.ml
+++ src/proof-system/codegen/ocaml_prelude.ml
@@ -1,45 +1,8 @@
 (* prelude *)
 
-module Identifier = struct
-  type t =
-    | I of int
-    | S of string
-    | L of int list
-
-  let equal : t -> t -> bool = ( = )
-
-  let gen =
-    let n = Atomic.make 0 in
-    fun [@inline] () -> Atomic.fetch_and_add n 1
-end
-
-module Make_id () : sig
-  type t = private Identifier.t
-
-  val make : Identifier.t -> t
-  val equal : t -> t -> bool
-end = struct
-  type t = Identifier.t
-
-  let equal = Identifier.equal
-  let make = Fun.id
-end
-
 type fn = string
 
-module Cbor_encoder = struct
-  type 'enc t = {
-    bool: 'enc -> bool -> unit;
-    int64: 'enc -> int64 -> unit;
-    string: 'enc -> string -> unit;
-    bytes: 'enc -> string -> unit;
-    begin_list: 'enc -> int -> unit;
-    begin_dict: 'enc -> int -> unit;
-  }
-end
-
-type 'a encoder = { encode: 'enc. 'enc Cbor_encoder.t -> 'enc -> 'a -> unit }
-[@@unboxed]
-(** An encoder to CBOR *)
+type ('a, 'res) encoder = Cbor_enc.t -> 'a -> unit
+(** An encoder to CBOR, with a phantom type *)
 
 (* end prelude *)
