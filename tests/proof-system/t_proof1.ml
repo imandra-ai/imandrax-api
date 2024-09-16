fe3d06b9da01e9d29e477cc49b9cd4179172aa4f
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-09-16 14:06:53 -0400

    test: basic test for proof encoding

---
+++ tests/proof-system/t_proof1.ml
@@ -0,0 +1,32 @@
+module P = Imandra_proof_system_encode
+module E_cbor = Imandra_proof_system_encode_cbor
+
+let () =
+  let buf = Buffer.create 32 in
+  let enc = E_cbor.encoder in
+
+  let total_bytes = ref 0 in
+  Printf.printf "start proof\n";
+
+  (* make an output that shows an hexdump per entry *)
+  let output_entry buf =
+    let str = Buffer.contents buf in
+    total_bytes := !total_bytes + String.length str;
+    let cbor, _ = CBOR.Simple.decode_partial str in
+    Printf.printf "entry (%dB): %s\n" (String.length str)
+      (CBOR.Simple.to_diagnostic cbor);
+    (* (CBOR.SiHex.hexdump_s @@ Hex.of_string @@ Buffer.contents buf); *)
+    Buffer.clear buf
+  in
+  let out =
+    P.Output.(Out { st = buf; enc; output_entry; gen_id = make_gen_id () })
+  in
+
+  let id_bool = P.ty_app out "bool" [] in
+  let id_true = P.t_bool out true in
+
+  let id_x = P.t_var out (P.var_mk out "x" id_bool) in
+
+  (* let id_x_eq_x = P.t_app *)
+  Printf.printf "proof done (%d B total)\n%!" !total_bytes;
+  ()
