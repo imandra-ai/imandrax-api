b8d11aade88622a3eae67ebd8044689a61128138
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-09-27 11:06:56 -0400

    refactor: rename version to api_types_version in Versioning

--- src/core/versioning.ml
+++ src/core/versioning.ml
@@ -1,11 +1,12 @@
 (** API versioning.
 
-    This version number {b MUST} be changed every time we modify the types. *)
+    This version number (in [API_TYPES_VERSION])
+    {b MUST} be changed every time we modify the types. *)
 
 (* for now we are conservative and use over-specific versions. *)
 
-(** The main version. See [VERSION]. *)
-let version : string = Version_.version
+(** The main version. See [API_TYPES_VERSION] file. *)
+let api_types_version : string = Version_.version
 
 (** Full git version *)
 let git_version = Gitid.rev
