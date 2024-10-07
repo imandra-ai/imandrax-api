c9790322775684319218d243178d2debc21e6dfd
Christoph M. Wintersteiger <christoph@imandra.ai>
2024-10-07 20:53:40 +0100

    Fix real consts in models

--- src/core/const.ml
+++ src/core/const.ml
@@ -12,7 +12,11 @@ let show = function
   | Const_float r -> string_of_float r
   | Const_string s -> Printf.sprintf "%S" s
   | Const_z x -> Z.to_string x
-  | Const_q x -> Q.to_string x
+  | Const_q x ->
+    if Z.equal x.den Z.one then
+      Z.to_string x.num ^ ".0"
+    else
+      "(" ^ Z.to_string x.num ^ ".0 /. " ^ Z.to_string x.den ^ ".0)"
   | Const_real_approx s -> s
   | Const_uid uid -> Uid.show uid
   | Const_bool b -> string_of_bool b
