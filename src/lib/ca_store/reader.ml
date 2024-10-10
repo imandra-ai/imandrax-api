4f4a53091419fb866fde74c4001e70becb703c25
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-10-10 10:15:27 -0400

    feat CA store reader: provide  a way to iterate over keys

--- src/lib/ca_store/reader.ml
+++ src/lib/ca_store/reader.ml
@@ -13,6 +13,7 @@ class type t = object
   method find_l : Key.t list -> (Key.t * string option) list
   method find_l_in_order : Key.t list -> string option list
   method find_opt : Key.t -> string option
+  method iter_keys : 'a. (Key.t Iter.t -> 'a) -> 'a
 end
 
 class dummy : t =
@@ -23,11 +24,13 @@ class dummy : t =
     method find_opt _ = None
     method find_l _ = []
     method find_l_in_order _ = []
+    method iter_keys : 'a. (Key.t Iter.t -> 'a) -> 'a = fun f -> f Iter.empty
   end
 
 let[@inline] mem (self : #t) (ks : Key.t list) : Basic_bv.t = self#mem ks
 let[@inline] find_opt (self : #t) k : _ option = self#find_opt k
 let[@inline] find_l (self : #t) ks : _ list = self#find_l ks
+let[@inline] iter_keys (self : #t) f = self#iter_keys f
 
 (** Find all keys, in the same order, or raise *)
 let[@inline] find_l_in_order (self : #t) ks : string option list =
