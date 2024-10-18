b85f89b2e1ed72fcaac98e0dad672676f0d98767
Nicola Mometto <nicola@imandra.ai>
2024-10-18 12:36:18 +0100

    chore: changes for twirp

--- src/lib/cir/decomp.ml
+++ src/lib/cir/decomp.ml
@@ -1,11 +1,11 @@
 open Imandrax_api
 
-module Region = struct
-  type status =
-    | Unknown
-    | Feasible of Model.t
-  [@@deriving show, twine, typereg] [@@typereg.name "Region.status"]
+type status =
+  | Unknown
+  | Feasible of Model.t
+[@@deriving show, twine, typereg]
 
+module Region = struct
   type t = {
     constraints: Term.t list;
     invariant: Term.t;
@@ -15,7 +15,7 @@ module Region = struct
 end
 
 type t = {
-  f_id: Uid.t;
+  f_id: Imandrax_api.Uid.t;
   f_args: Var.t list;
   regions: Region.t list;
 }
