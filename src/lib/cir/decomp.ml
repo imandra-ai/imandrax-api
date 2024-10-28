d8866c5163b21024850557483d25e6e1f2f03031
Nicola Mometto <nicola@imandra.ai>
2024-10-28 13:40:08 +0000

    feat: add rule_specs, basis as set

--- src/lib/cir/decomp.ml
+++ src/lib/cir/decomp.ml
@@ -3,7 +3,8 @@ open Imandrax_api
 type t = {
   f_id: Imandrax_api.Uid.t;
   assuming: Imandrax_api.Uid.t option;
-  basis: Imandrax_api.Uid.t list;
+  basis: Imandrax_api.Uid_set.t;
+  rule_specs: Imandrax_api.Uid_set.t;
   prune: bool;
 }
 [@@deriving show, twine, typereg]
