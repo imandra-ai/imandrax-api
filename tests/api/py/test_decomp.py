5c8f193380517d5c2db3734751f33ba020ba344a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-12-11 12:07:53 -0500

    test python

---
+++ tests/api/py/test_decomp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+with open('decomp.iml', 'r') as file:
+    data = file.read()
+
+import imandrax_api
+c = imandrax_api.Client('http://localhost:8000')
+c.eval_src(data)
+d = c.decompose(name='run_cycle', basis=['h_639909746', 'h_1617160941', 'h_767026065', 'h_1551930034'], timeout=480, prune=False)
+print(d)
+print("done, exiting")
