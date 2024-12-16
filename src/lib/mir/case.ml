cecc43d906cb730497f9153cc2f9107808dc151b
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-12-16 11:42:52 -0500

    duh no case in mir

--- src/lib/mir/case.ml
+++
@@ -1,5 +0,0 @@
-include Imandrax_api.Case_poly
-
-type 't t = ('t, Var.t, Applied_symbol.t) t_poly
-[@@deriving twine, typereg, eq, show { with_path = false }, map, iter]
-(** Case in a shallow pattern match *)
