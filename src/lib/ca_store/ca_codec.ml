5b6ed8ada3043b84550c8fd02edd1bc4ae4ec1d2
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-12-18 14:14:55 -0500

    feat castore: add Ca_codec.t, with decoding state

---
+++ src/lib/ca_store/ca_codec.ml
@@ -0,0 +1,35 @@
+(** Codec with complex deserialization state *)
+
+type ('st, 'a) t =
+  | C : {
+      codec: 'a Codec.t;
+      init: Imandrakit_twine.Decode.t -> 'res -> unit;
+      with_setup: 'a. 'st -> ('res -> 'a) -> 'a;
+    }
+      -> ('st, 'a) t
+
+let name (C self) = self.codec.name
+
+let of_codec codec : (unit, _) t =
+  C { codec; init = (fun _ () -> ()); with_setup = (fun () f -> f ()) }
+
+let of_codec_with_locked_res ~(init : Imandrakit_twine.Decode.t -> 'res -> unit)
+    codec : ('res Lock.t, _) t =
+  C
+    {
+      codec;
+      init;
+      with_setup =
+        (fun lres f ->
+          let@ res = Lock.with_lock lres in
+          f res);
+    }
+
+let encode_to_string (C self : (_, 'a) t) (x : 'a) : string =
+  Imandrakit_twine.Encode.encode_to_string self.codec.enc x
+
+let decode_string (C self : ('res, 'a) t) (res : 'res) (str : string) : 'a =
+  let@ res = self.with_setup res in
+  Imandrakit_twine.Decode.decode_string
+    ~init:(fun dec -> self.init dec res)
+    self.codec.dec str
