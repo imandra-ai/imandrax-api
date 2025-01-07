cd8a40e9438611c8320ba11610a7184c03ce62e0
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-01-07 16:03:32 -0500

    feat mir: tactic takes a `Top_fun.t`, not a  term
    a tactic is a lambda!

--- src/lib/mir/tactic.ml
+++ src/lib/mir/tactic.ml
@@ -1,4 +1,5 @@
 include Imandrax_api_common.Tactic
 
-type t = Term.t t_poly [@@deriving show { with_path = false }, twine, typereg]
+type t = Top_fun.t t_poly
+[@@deriving show { with_path = false }, twine, typereg]
 (** Tactic used to solve a PO *)
