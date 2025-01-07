1b28222042c5156306334e4ed426465f7dcf52f4
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-01-07 16:10:52 -0500

    tuple

--- src/lib/common/tactic.ml
+++ src/lib/common/tactic.ml
@@ -2,5 +2,5 @@
 type ('ty, 'term) t_poly =
   | Default_termination of { basis: Imandrax_api.Uid_set.t }
   | Default_thm
-  | Term of 'ty Var.t_poly list * 'term
+  | Term of ('ty Var.t_poly list * 'term)
 [@@deriving show { with_path = false }, map, iter, twine, typereg]
