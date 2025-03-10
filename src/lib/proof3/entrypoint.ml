4117607c8bc0d9f719593daf93b32afb643546d5
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-03-10 09:19:15 -0400

    modify entrypoint

--- src/lib/proof3/entrypoint.ml
+++ src/lib/proof3/entrypoint.ml
@@ -1,7 +1,7 @@
 type t = {
-  last_steps: Types.proof_step list;
-  last_deep_steps: Types.deep_proof_step list;
-  tree_leaves: Types.deep_proof_tree list;
+  last_steps: Types.proof_step Imandrakit_twine.offset_for list;
+  last_deep_steps: Types.deep_proof_step Imandrakit_twine.offset_for list;
+  tree_leaves: Types.deep_proof_tree Imandrakit_twine.offset_for list;
 }
 [@@deriving eq, twine, make, typereg, show { with_path = false }]
 (** This is the entrypoint into a proof file, from which we explore proof steps,
