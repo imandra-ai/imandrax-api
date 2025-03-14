9a40a75b07401deaab70c507480c73c40d66d9d1
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-03-13 22:04:28 -0400

    feat castore.reader: remove `mem`
    not compatible with LRU

--- src/lib/ca_store/reader.ml
+++ src/lib/ca_store/reader.ml
@@ -6,10 +6,6 @@ open Common_
 class type t = object
   inherit Core_classes.named
   inherit Core_classes.with_stats
-
-  method mem : Key.t list -> Basic_bv.t
-  (** Return a bitset where bit [i] is true if key [i] is present *)
-
   method find_l : Key.t list -> (Key.t * string option) list
   method find_l_in_order : Key.t list -> string option list
   method find_opt : Key.t -> string option
@@ -20,14 +16,12 @@ class dummy : t =
   object
     method name = "dummy"
     method add_stats _ = ()
-    method mem l = Basic_bv.create (List.length l)
     method find_opt _ = None
     method find_l _ = []
     method find_l_in_order _ = []
     method iter_keys : 'a. (Key.t Iter.t -> 'a) -> 'a = fun f -> f Iter.empty
   end
 
-let[@inline] mem (self : #t) (ks : Key.t list) : Basic_bv.t = self#mem ks
 let[@inline] find_opt (self : #t) k : _ option = self#find_opt k
 let[@inline] find_l (self : #t) ks : _ list = self#find_l ks
 let[@inline] iter_keys (self : #t) f = self#iter_keys f
