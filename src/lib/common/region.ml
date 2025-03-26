db0e690400209e3fe6f2e155c190c51db7ce6819
Nicola Mometto <nicola@imandra.ai>
2025-03-26 15:25:09 +0000

    feat: add structured meta to regions, remove regions_str from proto

--- src/lib/common/region.ml
+++ src/lib/common/region.ml
@@ -1,11 +1,25 @@
+module Util_twine_ = Imandrax_api.Util_twine_
+
 type ('term, 'ty) status =
   | Unknown
   | Feasible of ('term, 'ty) Model.t_poly
 [@@deriving show, twine, map, iter, typereg]
 
+type 'term meta =
+  | Null
+  | Bool of bool
+  | Int of Util_twine_.Z.t
+  | Real of Util_twine_.Q.t
+  | String of string
+  | Assoc of (string * 'term meta) list
+  | Term of 'term
+  | List of 'term meta list
+[@@deriving show, twine, map, iter, typereg]
+
 type ('term, 'ty) t_poly = {
   constraints: 'term list;
   invariant: 'term;
+  meta: (string * 'term meta) list;
   status: ('term, 'ty) status;
 }
 [@@deriving show, twine, map, iter, typereg]
