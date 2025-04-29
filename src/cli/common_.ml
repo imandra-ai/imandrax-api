60bcdc2012f812856bf08633ede762d4f72b99e6
Christoph M. Wintersteiger <christoph@imandra.ai>
2025-04-29 17:26:27 +0100

    Remove imandrax-api-cli (#82)

--- src/cli/common_.ml
+++
@@ -1,8 +0,0 @@
-module C = Imandrax_api_client_ezcurl
-module Fmt = CCFormat
-module Log = (val Logs.src_log (Logs.Src.create "imandrax.api.cli"))
-module Fut = Moonpool.Fut
-module Trace = Trace_core
-
-let ( let@ ) = ( @@ )
-let spf = Printf.sprintf
