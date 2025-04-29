a0164306a1f28cdd87e4a5e5ec5cc87b9cb2150e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-04-29 10:25:38 -0400

    feat(PO): add `upto` field

--- src/lib/common/proof_obligation.ml
+++ src/lib/common/proof_obligation.ml
@@ -10,6 +10,8 @@ type ('term, 'ty) t_poly = {
           This means we won't negate the goal. *)
   anchor: Imandrax_api.Anchor.t;
       (** An identifier linking this PO to some user-defined object *)
+  upto: Imandrax_api.Upto.t option;
+      (** For a [verify] statement, can be used for bounded verification *)
   timeout: int option;  (** Timeout *)
 }
 [@@deriving show { with_path = false }, twine, typereg, map, iter]
