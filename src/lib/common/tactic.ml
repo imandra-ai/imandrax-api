21e61e12ae9c3a386a13e730d2c9b270ba5c87c8
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-04-30 09:46:27 -0400

    feat: change `Tactic.t` to make default case more explicit

--- src/lib/common/tactic.ml
+++ src/lib/common/tactic.ml
@@ -1,6 +1,12 @@
 (** Tactic used to solve a PO *)
 type ('term, 'ty) t_poly =
-  | Default_termination of { basis: Imandrax_api.Uid_set.t }
-  | Default_thm
+  | Default_termination of {
+      max_steps: int;
+      basis: Imandrax_api.Uid_set.t;
+    }
+  | Default_thm of {
+      max_steps: int;
+      upto: Imandrax_api.Upto.t option;
+    }
   | Term of ('ty Var.t_poly list * 'term)
 [@@deriving show { with_path = false }, map, iter, twine, typereg]
