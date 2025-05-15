8711d35af7a2bb38da6bca33dc88e9cf502e5d2f
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-05-15 15:58:01 -0400

    feat anchor: sub-anchor, to point to specific subterms/hints

--- src/core/anchor.ml
+++ src/core/anchor.ml
@@ -3,4 +3,5 @@ type t =
   | Eval of int
   | Proof_check of t
   | Decomp of t
+  | Sub of t * int  (** A more precise location inside an anchor *)
 [@@deriving show { with_path = false }, eq, ord, twine, typereg]
