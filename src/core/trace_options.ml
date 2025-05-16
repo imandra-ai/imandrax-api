359ec740ac2c416ad9ed50a3d7089bf79e142ae3
Christoph M. Wintersteiger <christoph@imandra.ai>
2025-05-16 14:55:23 -0400

    trace options for server

--- src/core/trace_options.ml
+++ src/core/trace_options.ml
@@ -17,3 +17,19 @@ type t = {
 [@@deriving show, twine]
 
 let default () = { proof = NONE; detail = MINIMAL; file = None }
+
+module Server = struct
+  type client = t
+
+  type t = {
+    proof: proof_type;
+    detail: detail;
+  }
+  [@@deriving show, twine]
+
+  let default () =
+    let tod = default () in
+    { proof = tod.proof; detail = tod.detail }
+
+  let of_client (opts : client) = { proof = opts.proof; detail = opts.detail }
+end
