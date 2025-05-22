b0161b534086c47acd1ac109ad8bb4cd0019b2f2
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-05-22 14:57:25 -0400

    fix: sub-anchor should use a string
    indeed, our Uids change between the typing and MIR stage, so the
    typing AST will contain different Uids than later stages(!)
    as they have not been content-addressed yet. Better simply
    store the name.

--- src/core/sub_anchor.ml
+++ src/core/sub_anchor.ml
@@ -1,5 +1,6 @@
 type t = {
-  id: Uid.t;
+  fname: string;
+      (** name of the symbol containing this sub-anchor in its definition *)
   anchor: int;
 }
 [@@deriving eq, ord, show { with_path = false }, twine]
