bfab95e12b65ebeeb1946a40af512a7b28aa2429
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-12-18 12:04:53 -0500

    change the common Verify decl to include a quickcheck kind

--- src/lib/common/verify.ml
+++ src/lib/common/verify.ml
@@ -1,10 +1,16 @@
+type kind =
+  | K_verify
+  | K_instance
+  | K_quickcheck
+[@@deriving show { with_path = false }, eq, ord, enum, twine, typereg]
+
 type ('term, 'ty) t_poly = {
   verify_link: ('term, 'ty) Fun_def.t_poly;
       (** nameless function representing the property to check *)
   verify_simplify: bool;  (** simplify first? *)
   verify_nonlin: bool;  (** nonlinear real arithmetic? *)
   verify_upto: Imandrax_api.Upto.t option;
-  verify_is_instance: bool;
+  verify_kind: kind;
   verify_minimize: 'term list;  (** arithmetic terms to minimize in models *)
   verify_by: ('ty Var.t_poly list * 'term) option;
 }
