2849a0561e8360dd76ef3dce769910c2d8b8c7b8
Christoph M. Wintersteiger <christoph@imandra.ai>
2026-09-17 16:21:31 +0000

    Fix types of new OCaml client functions

--- src/client/core/imandrax_api_client_core.ml
+++ src/client/core/imandrax_api_client_core.ml
@@ -171,7 +171,7 @@ module Make (Fut : FUT) = struct
     self.rpc#rpc_call ~timeout_s API.Eval.Client.get_artifact_zip arg
 
   let get_artifact ?timeout_s (self : t) ~(task : API.task_id) ~(kind : string)
-      () : API.artifact Fut.t =
+      () : API.artifact_result Fut.t =
     let timeout_s = Option.value ~default:self.default_timeout_s timeout_s in
     let arg = API.make_artifact_get_query ~task_id:task ~kind () in
     self.rpc#rpc_call ~timeout_s API.Eval.Client.get_artifact arg
@@ -237,13 +237,13 @@ module Make (Fut : FUT) = struct
       self.rpc#rpc_call ~timeout_s API.Eval.Client.eval_code_snippet code
 
     let parse_term ?timeout_s (self : client) ~session ~code () :
-        API.artifact Fut.t =
+        API.artifact_result Fut.t =
       let timeout_s = Option.value ~default:self.default_timeout_s timeout_s in
       let code = API.make_code_snippet ~code ~session () in
       self.rpc#rpc_call ~timeout_s API.Eval.Client.parse_term code
 
     let parse_type ?timeout_s (self : client) ~session ~code () :
-        API.artifact Fut.t =
+        API.artifact_result Fut.t =
       let timeout_s = Option.value ~default:self.default_timeout_s timeout_s in
       let code = API.make_code_snippet ~code ~session () in
       self.rpc#rpc_call ~timeout_s API.Eval.Client.parse_type code
